242eaf7471c2cc92dcd2ffcd01b77e62b7a98bc2
dhodgson615 <dhodgson0615@gmail.com>
2025-07-03 22:24:55 -0400

    Added more function attributes

--- lambda.h
+++ lambda.h
@@ -14,6 +14,19 @@
 #define HIGHLIGHT        ESC "38;2;255;255;0m"
 #define ALWAYS_INLINE    __attribute__((always_inline))
 #define HOT              __attribute__((hot))
+#define PURE             __attribute__((pure))
+#define NOINLINE         __attribute__((noinline))
+#define NORETURN         __attribute__((noreturn))
+#define UNUSED           __attribute__((unused))
+#define INLINE           __attribute__((always_inline))
+#define COLD             __attribute__((cold))
+#define CONST            __attribute__((const))
+#define CONSTRUCTOR      __attribute__((constructor))
+#define DESTRUCTOR       __attribute__((destructor))
+#define ALIGNED(n)       __attribute__((aligned(n)))
+#define WARN_UNUSED      __attribute__((warn_unused_result))
+#define WEAK             __attribute__((weak))
+#define INIT_ARENA_SIZE  (1024 * 1024)
 
 
 typedef unsigned char    uchar;
