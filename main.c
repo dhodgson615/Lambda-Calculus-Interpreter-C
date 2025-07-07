93af599a37a7a2cafe613e6cd73dc6dacc651cde
dhodgson615 <dhodgson0615@gmail.com>
2025-07-06 20:30:37 -0400

    Update cleanup logic

--- main.c
+++ main.c
@@ -1,33 +1,31 @@
 #include "lambda.h"
 
-// External references to global variables defined in lambda.c
 expr *def_vals[N_DEFS];
 extern strbuf sb;
 
 int main(const int argc, char *argv[]) {
+    char *input = NULL;
+    expr *e = NULL;
+    int status = 1; // Default to error status
+
     // load δ-definitions
     for (int i = 0; i < N_DEFS; i++) {
         Parser dp = {def_src[i], 0, strlen(def_src[i])};
         def_vals[i] = parse(&dp);
         if (!def_vals[i]) {
             fprintf(stderr, "Failed to parse definition: %s\n", def_src[i]);
-            for (int k = 0; k < i; k++) free_expr(def_vals[k]);
-            sb_destroy(&sb);
-            exit(1);
+            goto cleanup;
         }
     }
     sb_init(&sb, MAX_PRINT_LEN);
-    char *input;
+
     if (argc > 1) {
         size_t L = 0;
         for (int i = 1; i < argc; i++) L += strlen(argv[i]) + 1;
         input = malloc(L + 1);
         if (!input) {
             perror("malloc for input");
-            // Cleanup def_vals
-            for (int i = 0; i < N_DEFS; i++) free_expr(def_vals[i]);
-            sb_destroy(&sb);
-            exit(1);
+            goto cleanup;
         }
         input[0] = '\0';
         for (int i = 1; i < argc; i++) {
@@ -42,11 +40,8 @@ int main(const int argc, char *argv[]) {
         const ssize_t chars_read = getline(&buf, &bufsize, stdin);
 
         if (chars_read == -1) { // Error
-            // Cleanup
             free(buf);
-            for (int i = 0; i < N_DEFS; i++) free_expr(def_vals[i]);
-            sb_destroy(&sb);
-            exit(1);
+            goto cleanup;
         }
 
         // Remove trailing newline
@@ -55,28 +50,26 @@ int main(const int argc, char *argv[]) {
         input = buf;
         if (!input) {
             perror("strdup for input");
-
-            // Cleanup def_vals
-            for (int i = 0; i < N_DEFS; i++) free_expr(def_vals[i]);
-            sb_destroy(&sb);
-            exit(1);
+            goto cleanup;
         }
     }
 
     Parser p = {input, 0, strlen(input)};
-    expr *e = parse(&p);
+    e = parse(&p);
     if (!e) {
-        free(input);
-        for (int i = 0; i < N_DEFS; i++) free_expr(def_vals[i]);
-        sb_destroy(&sb);
-        exit(1);
+        goto cleanup;
     }
     normalize(e);
+    e = NULL;
 
-    // cleanup
-    free(input);
-    for (int i = 0; i < N_DEFS; i++) free_expr(def_vals[i]);
+    // Success path
+    status = 0;
+
+    cleanup:
+        if (input) free(input);
+    for (int i = 0; i < N_DEFS; i++)
+        if (def_vals[i]) free_expr(def_vals[i]);
     sb_destroy(&sb);
 
-    return 0;
+    return status;
 }
