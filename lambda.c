7a57838d54ac3df79706500e7f16b27ca50b6d28
dhodgson615 <dhodgson0615@gmail.com>
2025-07-09 17:34:18 -0400

    Marked is_invalid_char() as PURE instead of CONST

--- lambda.c
+++ lambda.c
@@ -536,7 +536,7 @@ int parse_number(Parser *p) {
     return v;
 }
 
-CONST inline bool is_invalid_char(const Parser *p, const char c) {
+HOT PURE inline bool is_invalid_char(const Parser *p, const char c) {
     return (!c) || (c == '(') || (c == ')') || (c == '.') ||
             (isspace((uchar) c)) || ((p->i + 1 < p->n) &&
              ((uchar) p->src[p->i] == 0xCE) &&
