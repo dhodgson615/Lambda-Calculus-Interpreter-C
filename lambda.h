dec30630cf24b444ee55cce7a8ecaad9c1b8530c
dhodgson615 <dhodgson0615@gmail.com>
2025-07-21 12:25:49 -0400

    Added spaces to header

--- lambda.h
+++ lambda.h
@@ -9,27 +9,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_PRINT_LEN      (32 * 1024 * 1024)
-#define ESC                ("\x1b[")
-#define RESET              (ESC "0m")
-#define HIGHLIGHT          (ESC "38;2;255;255;0m")
-#define HOT                __attribute__((hot))
-#define PURE               __attribute__((pure))
-#define NOINLINE           __attribute__((noinline))
-#define NORETURN           __attribute__((noreturn))
-#define INLINE             __attribute__((always_inline))
-#define COLD               __attribute__((cold))
-#define CONST              __attribute__((const))
-#define CONSTRUCTOR        __attribute__((constructor))
-#define DESTRUCTOR         __attribute__((destructor))
-#define ALIGNED(n)         __attribute__((aligned(n)))
-#define WARN_UNUSED        __attribute__((warn_unused_result))
-#define WEAK               __attribute__((weak))
-#define INIT_ARENA_SIZE    (1024 * 1024)
-#define DEBUG              false
-#define PROFILE            false
-#define UNUSED             __attribute__((unused))
-#define DEAD               __attribute__((unused))
+#define MAX_PRINT_LEN          (32 * 1024 * 1024)
+#define ESC                    ("\x1b[")
+#define RESET                  (ESC "0m")
+#define HIGHLIGHT              (ESC "38;2;255;255;0m")
+#define HOT                    __attribute__((hot))
+#define PURE                   __attribute__((pure))
+#define NOINLINE               __attribute__((noinline))
+#define NORETURN               __attribute__((noreturn))
+#define INLINE                 __attribute__((always_inline))
+#define COLD                   __attribute__((cold))
+#define CONST                  __attribute__((const))
+#define CONSTRUCTOR            __attribute__((constructor))
+#define DESTRUCTOR             __attribute__((destructor))
+#define ALIGNED(n)             __attribute__((aligned(n)))
+#define WARN_UNUSED            __attribute__((warn_unused_result))
+#define WEAK                   __attribute__((weak))
+#define INIT_ARENA_SIZE        (1024 * 1024)
+#define DEBUG                  false
+#define PROFILE                false
+#define UNUSED                 __attribute__((unused))
+#define DEAD                   __attribute__((unused))
 
 typedef unsigned char      uchar;
 typedef unsigned char      byte;
