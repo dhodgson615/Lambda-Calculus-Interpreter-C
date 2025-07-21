87c1b060f773571a0b8b29a1e4c47cddeafb18e1
dhodgson615 <dhodgson0615@gmail.com>
2025-07-21 12:26:01 -0400

    Added spaces and more types

--- lambda.h
+++ lambda.h
@@ -31,12 +31,19 @@
 #define UNUSED                 __attribute__((unused))
 #define DEAD                   __attribute__((unused))
 
-typedef unsigned char      uchar;
-typedef unsigned char      byte;
-typedef unsigned short     word;
-typedef unsigned int       dword;
-typedef unsigned long long qword;
-typedef uint8_t            uint8;
+typedef unsigned char          uchar;
+typedef unsigned char          byte;
+typedef unsigned short         word;
+typedef unsigned int           dword;
+typedef unsigned long long     qword;
+typedef uint8_t                uint8;
+typedef uint16_t               uint16;
+typedef uint32_t               uint32;
+typedef uint64_t               uint64;
+typedef int8_t                 int8;
+typedef int16_t                int16;
+typedef int32_t                int32;
+typedef int64_t                int64;
 
 /**
  * @brief              Memory arena structure for temporary allocations.
