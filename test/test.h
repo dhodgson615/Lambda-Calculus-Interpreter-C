405aff6b9ebb2180160a55ae4f5ec01220fc23be
dhodgson615 <dhodgson0615@gmail.com>
2025-07-21 15:52:18 -0400

    Created test.h

---
+++ test/test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "../src/expr.h"
+#include <stdbool.h>
+#include <string.h>
+
+#define TEST(name) static void test_##name(void)
+#define RUN_TEST(name) do {     \
+printf("Running %-32s", #name); \
+test_##name();                  \
+printf("[ PASS ]\n");           \
+} while(0)
+
+#endif //TEST_H
