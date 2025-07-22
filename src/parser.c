11dd1b126bc35f018f33d51e3b1880bad2161fae
dhodgson615 <dhodgson0615@gmail.com>
2025-07-22 00:57:03 -0400

    Made uint instead of size_t

--- src/parser.c
+++ src/parser.c
@@ -130,7 +130,7 @@ int parse_number(Parser *p) {
 
 HOT INLINE char *parse_varname(Parser *p) {
     skip_whitespace(p);
-    const size_t start = p->i;
+    const unsigned int start = p->i;
     while (p->i < p->n && !is_invalid_char(p, peek(p))) p->i++;
     size_t len = p->i - start;
     if (len == 0) {
