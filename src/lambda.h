61bdbdc769003113e58a2d49b55e57545a08b6ef
dhodgson615 <dhodgson0615@gmail.com>
2025-07-23 11:42:46 -0400

    Removed redundant macros

--- src/lambda.h
+++ src/lambda.h
@@ -16,29 +16,6 @@
 
 #include <stdbool.h>
 
-#define MAX_PRINT_LEN          (32 * 1024 * 1024)
-#define ESC                    ("\x1b[")
-#define RESET                  (ESC "0m")
-#define HIGHLIGHT              (ESC "38;2;255;255;0m")
-#define HOT                    __attribute__((hot))
-#define PURE                   __attribute__((pure))
-#define NOINLINE               __attribute__((noinline))
-#define NORETURN               __attribute__((noreturn))
-#define INLINE                 __attribute__((always_inline))
-#define COLD                   __attribute__((cold))
-#define CONST                  __attribute__((const))
-#define CONSTRUCTOR            __attribute__((constructor))
-#define DESTRUCTOR             __attribute__((destructor))
-#define ALIGNED(n)             __attribute__((aligned(n)))
-#define WARN_UNUSED            __attribute__((warn_unused_result))
-#define WEAK                   __attribute__((weak))
-#define INIT_ARENA_SIZE        (1024 * 1024)
-#define DEBUG                  false
-#define PROFILE                false
-#define UNUSED                 __attribute__((unused))
-#define DEAD                   __attribute__((unused))
-#define UNREACHABLE            __attribute__((unreachable))
-
 /**
  * @brief              Delta definitions.
  */
