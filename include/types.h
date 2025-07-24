ab4626804ed880610bb1150e709f6d1aedff5587
dhodgson615 <dhodgson0615@gmail.com>
2025-07-24 18:06:34 -0400

    Moved typedefs

--- include/types.h
+++ include/types.h
@@ -4,29 +4,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-typedef unsigned char          uchar;
-typedef unsigned char          byte;
-typedef unsigned short         word;
-typedef unsigned int           dword;
-typedef unsigned long long     qword;
-typedef uint8_t                uint8;
-typedef uint16_t               uint16;
-typedef uint32_t               uint32;
-typedef uint64_t               uint64;
-typedef int8_t                 int8;
-typedef int16_t                int16;
-typedef int32_t                int32;
-typedef int64_t                int64;
-
-/**
- * @brief              Memory arena structure for temporary allocations.
- */
-typedef struct arena {
-    uint8        *buf;
-    size_t        cap;
-    size_t        pos;
-} arena;
-
 /**
  * @brief              Variable set structure.
  */
