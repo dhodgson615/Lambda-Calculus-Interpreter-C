fa2e9a56e9f58b3921d8c1e59521013423decf9d
dhodgson615 <dhodgson0615@gmail.com>
2025-07-24 18:06:40 -0400

    MOved typedefs

--- include/types.h
+++ include/types.h
@@ -12,13 +12,34 @@ typedef struct VarSet {
     int            c;
 } VarSet;
 
-/**
- * @brief              RGB color structure for pretty printing.
- */
-typedef struct color {
-    uint8          r;
-    uint8          g;
-    uint8          b;
-} color;
+typedef enum {
+    VAR_expr, ABS_expr, APP_expr
+} exprType;
+
+typedef struct expr {
+    exprType       type;
+    char          *var_name;
+    char          *abs_param;
+    struct expr   *abs_body;
+    struct expr   *app_fn;
+    struct expr   *app_arg;
+} expr;
+
+typedef unsigned char          uchar;
+typedef unsigned char          byte;
+typedef unsigned short         word;
+typedef unsigned int           dword;
+typedef unsigned long long     qword;
+typedef uint8_t                uint8;
+typedef uint16_t               uint16;
+typedef uint32_t               uint32;
+typedef uint64_t               uint64;
+typedef int8_t                 int8;
+typedef int16_t                int16;
+typedef int32_t                int32;
+typedef int64_t                int64;
+typedef const char             cchar;
+typedef const expr             cexpr;
+typedef const int              cint;
 
 #endif /* TYPES_H */
